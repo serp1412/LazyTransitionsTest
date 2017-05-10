16213a7cdc1d6f8260dbe4f70c300111ce1f197b
Joe Fabisevich <Joe@fabisevi.ch>
2017-05-10 18:54:32 -0400

    Initial Commit

---
+++ LazyTransitionsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LazyTransitionsTest
+//
+//  Created by Joe Fabisevich on 5/10/17.
+//  Copyright © 2017 Joe Fabisevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
